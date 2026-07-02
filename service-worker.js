1a62ec49901dd03017e655ac2cc80afc474a16e7
SYSTEMTOOLBOX <cncsystem.mec@gmail.com>
2026-07-02 11:07:04 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'cnc-toolbox-v1-3-vera';
+// CNC Toolbox v1.4
+const CACHE_NAME = 'cnc-toolbox-v1-4-vera';
 const FILES = ['./','./index.html','./manifest.webmanifest','./service-worker.js','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
